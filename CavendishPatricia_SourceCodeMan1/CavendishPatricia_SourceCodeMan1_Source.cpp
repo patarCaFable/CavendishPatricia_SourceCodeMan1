18ff93d3af2784396125b4edcb877d6faf8caf13
Cavendish <patricia.cavendish@sinclair.edu>
2022-03-16 13:02:52 -0400

    Add project files.

---
+++ CavendishPatricia_SourceCodeMan1/CavendishPatricia_SourceCodeMan1_Source.cpp
@@ -0,0 +1,13 @@
+/*Patricia Cavendish
+*CIS 1202 501
+*Source Code Management 1
+*March 20, 2022
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+	return 0;
+}
